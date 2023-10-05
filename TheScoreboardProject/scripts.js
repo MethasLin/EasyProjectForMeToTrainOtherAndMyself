b47ba202d77021468ac31f132c7fcb3b89951f5f
MethasLin <methasitlin@gmail.com>
2023-10-05 16:27:39 +0700

    update dynamic thingy

--- TheScoreboardProject/scripts.js
+++ TheScoreboardProject/scripts.js
@@ -2,10 +2,9 @@ for (let i = 1; i <= 17; i++) {
     const element = document.querySelector(`.top_${i}`);
     const rect = element.getBoundingClientRect();
     
+    // Log the position and dimensions of the element
     console.log(`Position and dimensions of top_${i}:`);
     console.log(`Top: ${rect.top}px`);
     console.log(`Left: ${rect.left}px`);
-    console.log(`Width: ${rect.width}px`);
-    console.log(`Height: ${rect.height}px`);
     console.log('---------------------------------------');
 }
