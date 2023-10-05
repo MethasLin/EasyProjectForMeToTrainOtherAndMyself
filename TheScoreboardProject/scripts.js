440ecf02cafac52e5c6777b0e4b78873ab36fe31
MethasLin <methasitlin@gmail.com>
2023-10-05 15:38:40 +0700

    Update scripts.js

--- TheScoreboardProject/scripts.js
+++ TheScoreboardProject/scripts.js
@@ -2,7 +2,6 @@ for (let i = 1; i <= 17; i++) {
     const element = document.querySelector(`.top_${i}`);
     const rect = element.getBoundingClientRect();
     
-    // Log the position and dimensions of the element
     console.log(`Position and dimensions of top_${i}:`);
     console.log(`Top: ${rect.top}px`);
     console.log(`Left: ${rect.left}px`);
